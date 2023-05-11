bb181dc6e92aa336e1fa1e7211a4b7bdba322ae3
Walfenstaine <Pavlik-gureev@mail.ru>
2023-05-11 17:10:54 +0300

    8988

--- Assets/Skripts/EnemiAI.cs
+++ Assets/Skripts/EnemiAI.cs
@@ -4,58 +4,32 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemiAI : MonoBehaviour {
-	public float speed, stopDist, spector;
+	public float speed, stopDist;
 	public Animator anim;
-	public Transform player;
+	public Transform[] target;
+    private int num;
 	public NavMeshAgent agent;
-    private float radius;
-    public LayerMask mask;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            player = other.transform;
-        }
-    }
-    public void Serching()
-    {
-        Collider[] serch = Physics.OverlapSphere(transform.position, radius, mask);
-        if (serch.Length > 0)
-        {
-            player = serch[0].transform;
-        }
-        if (radius < spector)
-        {
-            radius += spector * 10 * Time.deltaTime;
-        }
-        else
-        {
-            radius = 0;
-        }
-    }
+
+
     void Update () {
         anim.SetFloat ("Speed", agent.velocity.magnitude/speed);
-        if (player != null)
+        if (Vector3.Distance(target[num].position, transform.position) <= stopDist)
         {
-            if (Vector3.Distance(player.position, transform.position) <= stopDist)
+            if (num < target.Length-1)
             {
-                agent.speed = 0;
-                anim.SetBool("Kik", true);
+                num += 1;
             }
             else {
-                agent.speed = speed;
-                anim.SetBool("Run", true);
-                anim.SetBool("Kik", false);
+                num = 0;
             }
-            agent.destination = player.position;
         }
         else
         {
-            Serching();
-            agent.speed = 0;
-            anim.SetBool("Run", false);
-            anim.SetBool("Kik", false);
+            //agent.speed = speed;
+            //anim.SetBool("Run", true);
         }
-	}
+        agent.speed = speed;
+        agent.destination = target[num].position;
+    }
 }
