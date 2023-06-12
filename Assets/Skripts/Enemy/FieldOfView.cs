ac4bd4eba5dc8f376ee54f42d65dbb0881e78c54
Павел Петров <pavelkpetrov@mail.ru>
2023-06-12 23:52:28 +0300

    fix

--- Assets/Skripts/Enemy/FieldOfView.cs
+++ Assets/Skripts/Enemy/FieldOfView.cs
@@ -5,35 +5,57 @@ using UnityEngine;
 public class FieldOfView : MonoBehaviour
 {
 
+    [SerializeField] private float distance;
+    [SerializeField] private float angle;
+
+    private MeshFilter fOVMeshFilter;
+
+
+
+
     void Start()
     {
-        CreateMesh();
-        StartCoroutine(SearchForPlayer());
+        fOVMeshFilter = GetComponent<MeshFilter>();
+        fOVMeshFilter.mesh = CreateMesh();
+
+
+        //StartCoroutine(SearchForPlayer());
     }
 
-    IEnumerator SearchForPlayer()
+    Mesh CreateMesh()
     {
-        while (true)
+        Mesh myMesh = new Mesh();
+        myMesh.name = "LOSMesh";
+
+        Vector3 shiftLeft = new Vector3(-Mathf.Sin(angle),0, Mathf.Cos(angle));
+        Vector3 shiftRight = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle));
+
+        myMesh.vertices = new Vector3[] {
+            Vector3.zero, distance*shiftLeft, distance*new Vector3(0,0,Mathf.Cos(angle)), distance*shiftRight
+        };
+
+        myMesh.triangles = new int[] {
+            2,1,0,3,2,0
+        };
+
+        return myMesh;
+    }
+
+
+
+    /*    IEnumerator SearchForPlayer()
         {
-            CheckFieldOfView();
-            ReCreateMesh();
-            yield return new WaitForSeconds(.2f);
+            while (true)
+            {
+                CheckFieldOfView();
+                yield return new WaitForSeconds(.2f);
 
+            }
         }
-    }
 
-    void CreateMesh()
-    {
+        void CheckFieldOfView()
+        {
 
-    }
+        }*/
 
-    void CheckFieldOfView()
-    {
-
-    }
-
-    void ReCreateMesh()
-    {
-
-    }
 }
